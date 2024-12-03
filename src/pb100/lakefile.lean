106dc47ab9a4b9a0c7d68333546ba52d0fa9b250
Derek Rhodes <physci@gmail.com>
2024-12-03 11:08:10 -0500

    set_option autoImplcity false for whole project

--- src/pb100/lakefile.lean
+++ src/pb100/lakefile.lean
@@ -4,12 +4,17 @@ open Lake DSL
 package «pb100» where
   -- Settings applied to both builds and interactive editing
   leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩,
+    ⟨`pp.unicode.fun, true⟩
   ]
+
   -- add any additional package configuration options here
+
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
+
+
 
 @[default_target]
 lean_lib «Pb100» where
