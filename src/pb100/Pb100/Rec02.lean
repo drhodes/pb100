3c21270effcf7310d0a51691c5e20fa5f1aacdcd
Derek Rhodes <physci@gmail.com>
2024-12-14 13:41:35 -0500

    finishes the recitation 2 proof, set X has supremum (sup X)^3 = 2

--- src/pb100/Pb100/Rec02.lean
+++ src/pb100/Pb100/Rec02.lean
@@ -1,17 +1,23 @@
 import Mathlib
+
+import Pb100.Rec02Util
+
 set_option autoImplicit false
 
 namespace rec_2
 
--- comments that start with † is remixed from 18.100A, please see LICENSE for
+-- comments that start with † are remixed from 18.100A, please see LICENSE for
 -- more details.
 
 section p2
 
-@[reducible]
-def bdd (s : Set ℝ) := BddBelow s ∧ BddAbove s
-
 def X := {a : ℝ | 0 < a ∧ a ^ 3 < 2 }
+
+lemma one_le_r_of_lub (r : ℝ) (h₀ : IsLUB X r) : 1 ≤ r := by
+  have ⟨h₁, _⟩ := h₀
+  dsimp [IsLUB, X, upperBounds] at h₁
+  apply h₁
+  aesop
 
 lemma lem₁ {a b c : ℝ} (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) : a < b ↔ a ^ c < b ^ c := by
   refine Iff.symm (Real.rpow_lt_rpow_iff ?_ ?_ hc)
@@ -57,79 +63,10 @@ lemma part1 : bdd X := by
     · linarith
 
 
-lemma bs_mul_le {a b : ℝ} (c : ℝ) (hc : 0 < c): a ≤ b ↔ c * a ≤ c * b := by
-  exact Iff.symm (mul_le_mul_iff_of_pos_left hc)
-
-lemma bs_mul_lt {a b : ℝ} (c : ℝ) (hc : 0 < c): a < b ↔ c * a < c * b := by
-  exact Iff.symm (mul_lt_mul_iff_of_pos_left hc)
-
-lemma mul_one_rhs {a b : ℝ} : a ≤ b ↔ a ≤ 1 * b := by ring_nf
-
-lemma both_not_least (a b : ℝ) (X: Set ℝ) (h : a ≠ b) : (IsLeast X a ∧ IsLeast X b) → False := by
-  intro ⟨⟨ha₁, ha₂⟩, ⟨hb₁, hb₂⟩⟩
-  have hc₁ := @hb₂ a ha₁
-  have hc₂ := @ha₂ b hb₁
-  have : a = b := by linarith
-  contradiction
-
-lemma zero_lt_of_lt_cube (r : ℝ) (hr : 0 < r ^ 3) : 0 < r := by
-  have : Odd 3 := by exact Nat.odd_iff.mpr rfl
-  exact (Odd.pow_pos_iff this).mp hr
-
-lemma cube_lt_cube_of_lt (a b : ℝ) (ha₁ : a < b) (ha₂ : 0 < a) : a ^ 3 < b ^ 3 := by
-  have : 0 < b := by exact gt_trans ha₁ ha₂
-  have : 0 < a ^ 3 := by positivity
-  have : 0 < b ^ 3 := by positivity
-  calc a^3
-    _= a * a * a := by ring
-    _< b * b * b := by rel [ha₁]
-    _= b ^ 3 := by ring
-
-lemma lt_of_one_cube_lt_cube (a b : ℝ) (ha₁ : a ^ 3 < b ^ 3) (ha₂ : 1 < a) : a < b := by
-  have h₀ : 0 ≤ a := by linarith
-  have h₁ : 0 ≤ b := by
-    have := calc 0
-      _< a := by linarith
-      _= a * 1 * 1 := by ring
-      _< a * a * a := by rel [ha₂]
-      _= a^3 := by ring
-      _< b^3 := ha₁
-    have hz := zero_lt_of_lt_cube b this
-    linarith
-
-  have h₂ : 3 ≠ 0 := by norm_num
-  rw [← pow_lt_pow_iff_left h₀ h₁ h₂]
-  exact ha₁
-
-lemma lt_of_zero_cube_lt_cube (a b : ℝ) (ha₁ : a ^ 3 < b ^ 3) (ha₂ : 0 < a ^ 3) : a < b := by
-  have h₀ : 0 ≤ a := by
-    rw [le_iff_lt_or_eq]
-    left
-    apply zero_lt_of_lt_cube a ha₂
-
-  have h₁ : 0 ≤ b := by
-    have := calc 0
-      _< a^3 := by linarith
-      _< b^3 := ha₁
-    have hz := zero_lt_of_lt_cube b this
-    linarith
-
-  have h₂ : 3 ≠ 0 := by norm_num
-  rw [← pow_lt_pow_iff_left h₀ h₁ h₂]
-  exact ha₁
-
-
-
-lemma one_lt_of_one_lt_cube (r : ℝ) (hr : 1 < r ^ 3) : 1 < r := by
-  have : Odd 3 := by exact Nat.odd_iff.mpr rfl
-  have rsqpos : 0 ≤ r * r := by exact mul_self_nonneg r
-  have lt_cube := zero_lt_of_lt_cube r ?_
-  · nlinarith
-  · nlinarith
-
-theorem support_for_two (r : ℝ) (h₀ : IsLUB X r) (hc : 2 < r ^ 3) (hr₁ : 0 < r) (hr₂ : 1 < r) :
-  ∃ h, 0 < h ∧ 2 ≤ r ^ 3 - 3 * r ^ 2 * h := by
+theorem find_good_ε (r : ℝ) (h₀ : IsLUB X r) (hc : 2 < r ^ 3) (hr₁ : 0 < r) (hr₂ : 1 < r) :
+  ∃ ε, 0 < ε ∧ 2 ≤ r ^ 3 - 3 * r ^ 2 * ε := by
   --
+
   have ⟨h₁, h₂⟩ := h₀
   use (r ^ 3 - 2) / (3 * r ^ 2)
   constructor
@@ -172,89 +109,180 @@ theorem support_for_two (r : ℝ) (h₀ : IsLUB X r) (hc : 2 < r ^ 3) (hr₁ : 0
     norm_num
     exact hr₁
 
-theorem step21 (r : ℝ) (h₀ : IsLUB X r) : r ^ 3 ≤ 2 := by
+
+lemma step21 (r : ℝ) (h₀ : IsLUB X r) : r ^ 3 ≤ 2 := by
   -- † Assume for the sake of contradiction that 2 < r ^ 3.
 
-  have ⟨h₁, h₂⟩ := h₀
   by_contra hc
   push_neg at hc
 
+  -- prepare some facts to help show the contradiciton.
+
+  have hr₁ : 1 < r := by apply one_lt_of_one_lt_cube r; linarith
+  have hr₂ : 0 < r := by linarith
+
   -- † Let’s try to intuitively understand why this should give a contradiction.
-  -- Given this is the case, then we should be able to subtract a "small" h > 0
-  -- from r such that 2 < (r − h) ^ 3, then r − h is still an upper bound for X.
+  -- Given this is the case, then we should be able to subtract a "small" ε > 0
+  -- from r such that 2 < (r − ε) ^ 3, then r − ε is still an upper bound for X.
   -- This would be a contradiction, as the supremum is the least upper bound.
   -- However, we still need to make this proof more formal by explicitly saying
   -- what h is. to do this, consider the following:
 
-  dsimp [lowerBounds, upperBounds, X] at h₁ h₂
+  obtain ⟨ε, ⟨hh₀, hh⟩⟩ := find_good_ε r h₀ hc hr₂ hr₁
 
-  have hr₂ : 1 < r := by
-    have :=
-      calc 1
-        _< (2:ℝ) := by norm_num
-        _< r^3 := hc
-    apply one_lt_of_one_lt_cube r this
+  -- † notice that if we can find an ε such that 0 < r ^ 3 - 3 * r ^ 2 * ε then
+  -- we will have 2 < (r - ε) ^ 3, which would be a contradiction
 
-  have hr₁ : 0 < r := by linarith
-
-  --
-  obtain ⟨h, ⟨hh₀, hh⟩⟩ := support_for_two r h₀ hc hr₁ hr₂
-
-  have hh₃ : h < r / 3 := by
-    have h2 :=
+  let cubic_tail := 3 * r * ε ^ 2 - ε ^ 3
+  -- hc : 2 < r ^ 3
+  have contra : 2 < (r - ε) ^ 3 :=
     calc 2
-      _≤ r ^ 3 - 3 * r ^ 2 * h := hh
-      _= r ^ 2 * (r - 3 * h) := by ring
-    have h3 : 0 < r - 3 * h := by nlinarith
-    linarith
+      _≤ r ^ 3 - 3 * r ^ 2 * ε := hh
+      _= r ^ 3 - 3 * r ^ 2 * ε + (0) := by ring
+      _< r ^ 3 - 3 * r ^ 2 * ε + cubic_tail := by
+        have : 0 < cubic_tail := by simp [cubic_tail]; nlinarith
+        gcongr
+      _= (r - ε) ^ 3 := by ring
 
-  have hh₄ : 0 < 3 * r * h ^ 2 - h ^ 3 := by nlinarith
-
-  -- † notice that if we can find an h such that 2 ≤ r ^ 3 - 3 * r ^ 2 * h then
-  -- we will have 2 < (r - h) ^ 3, which would be a contradiction
-
-  have contra : 2 < (r - h) ^ 3 :=
-    calc 2
-      _≤ r ^ 3 - 3 * r ^ 2 * h := hh
-      _= r ^ 3 - 3 * r ^ 2 * h + (0) := by ring
-      _< r ^ 3 - 3 * r ^ 2 * h + (3 * r * h ^ 2 - h ^ 3) := by rel [hh₄]
-      _= (r - h) ^ 3 := by ring
-
-  -- now we have an element (r - h) in the upper bounds of X that is less than the
+  -- now we have an element (r - ε) in the upper bounds of X that is less than the
   -- supremum r.
 
-  have h₀' : ∃ p, p ∈ upperBounds X ∧ p < r := by
-    use r - h
+  have h₀' : r - ε ∈ upperBounds X ∧ r - ε < r := by
     constructor
     · dsimp [upperBounds]
       intro p ⟨hp₁, hp₂⟩
       have :=
       calc p ^ 3
         _< 2 := hp₂
-        _< (r - h) ^ 3 := contra
+        _< (r - ε) ^ 3 := contra
       have this₀ : 0 < p^3 := by positivity
-      have this₁ := lt_of_zero_cube_lt_cube p (r-h) this this₀
+      have this₁ := lt_of_zero_cube_lt_cube p (r-ε) this this₀
       rw [le_iff_lt_or_eq]
       left
       exact this₁
     · linarith
 
-  --
-  have ⟨s, ⟨hs₁, hs₂⟩⟩ := h₀'
+  -- build the contradiction.
+
+  have ⟨hs₁, hcon₁⟩ := h₀'
   have ⟨_, h₂'⟩ := h₀
-  have hs₃ := @h₂' s hs₁
-  have : ¬ s < r := by linarith
+  have hs₃ := @h₂' (r - ε) hs₁
+  have hcon₂ : ¬ (r - ε) < r := by linarith
+
   contradiction
 
 
-theorem step22 (r : ℝ) (h₀ : IsLUB X r) : 2 ≤ r ^ 3 := by
-  have ⟨h₁, h₂⟩ := h₀
-  sorry
+lemma asdfasdf (r : ℝ) (h₀ : IsLUB X r) : 1 < r := by
+  obtain ⟨h₁, _⟩ := h₀
+  dsimp [upperBounds] at h₁
+  have hx : (10/9) ∈ X := by
+    simp [X]
+    norm_num
+  have h := @h₁ (10/9) hx
+  nlinarith
 
-lemma part2 (r : ℝ) : IsLUB X r → r ^ 3 = 2 := by
+theorem find_good_ε₂ (r : ℝ) (h₀ : IsLUB X r) (hc : r ^ 3 < 2)
+    : ∃ ε, 0 < ε ∧ (r + ε) ^ 3 < 2 := by
+  --
+  let k := 10 -- α ∝ 1 / k
+  let ω := 2 - r ^ 3
+  let α := ω / (k * r ^ 2)
+  use α
+
+  have hr₁ : 1 < r := asdfasdf r h₀
+  have hr₂ : 0 < r := by linarith
+  have hω  : 0 < ω := by dsimp [ω]; linarith
+  have ok₁ : 0 < α := by dsimp [α]; aesop
+  have ok₃ : r ≠ 0 := by linarith
+
+  constructor
+  · exact ok₁
+  · -- (r + α) ^ 3 < 2
+
+    have need₁ : α < r := by
+      dsimp [α, ω]
+      have : 0 < (10 * r ^ 2) := by aesop
+      rw [div_lt_iff₀ this]
+      nlinarith
+
+    have piece₁ : 3 * (r * α ^ 2) < 3 * (r ^ 2 * α) := by
+      -- clean up with calc block?
+      calc 3 * (r * α ^ 2)
+        _= 3 * (r * α * α) := by ring
+        _< 3 * (r * r * α) := by rel [need₁]
+        _= 3 * (r ^2 * α) := by ring
+
+    have piece₂ : α ^ 3 < 3 * r ^ 2 * α := by
+      calc α ^ 3
+        _= α * α * α := by ring
+        _< r * r * α := by rel [need₁]
+        _= 1 * r ^ 2 * α := by ring
+        _< 3 * r ^ 2 * α := by
+          have : 1 < (3:ℝ) := by norm_num
+          rel [this]
+
+    have trip :=
+      calc 3 * r ^ 2 * α + 3 * r * α ^ 2 + α ^ 3
+        _< 3 * r ^ 2 * α + 3 * r ^ 2 * α + 3 * r ^ 2 * α := by linarith
+        _= 9 * r ^ 2 * α := by ring
+
+    have final : 9 * r ^ 2 * α < 2 - r ^ 3 := by
+      calc 9 * r ^ 2 * α
+        _= 9 * r ^ 2 * ω / (k * r ^ 2) := by ring
+        _= 9 * r ^ 2 * ω / k * 1 / r ^ 2 := by ring
+        _= 9 * r ^ 2 * (1 / r ^ 2) * ω / k  := by ring
+        _= 9 * (r ^ 2 / r ^ 2) * ω / k  := by ring
+        _= 9 * (1) * ω / k  := by aesop
+        _= 9 * ω / 10  := by ring
+        _= (9 / 10) * ω := by ring
+        _= (9 / 10) * (2 - r ^ 3) := by ring
+        _< 1 * (2 - r ^ 3) := by
+          have : (9:ℝ) / 10 < 1 := by linarith
+          rel [this]
+        _= 2 - r ^ 3 := by ring
+
+    calc (r + α) ^ 3
+      _= r ^ 3 + (3 * r ^ 2 * α + 3 * r * α ^ 2 + α ^ 3) := by ring
+      _< r ^ 3 + (9 * r ^ 2 * α) := by rel [trip]
+      _< 2 := by linarith
+
+
+-- def X := {a : ℝ | 0 < a ∧ a ^ 3 < 2 }
+theorem step22 (r : ℝ) (h₀ : IsLUB X r) : 2 ≤ r ^ 3 := by
+   -- Assume for the sake of contradiction that r ^ 3 < 2.
+  by_contra hc
+  push_neg at hc
+  have ⟨h₁, h₂⟩ := h₀
+
+  obtain ⟨ε, ⟨hε₁, hε₂⟩⟩ := find_good_ε₂ r h₀ hc
+
+  -- now we have an element (sup + ε) in the Set X but is greater than the
+  -- sup r?
+  have hr : 0 < r := by
+    have := asdfasdf r h₀
+    linarith
+
+  have h₀' : r + ε ∈ X ∧ r + ε ∈ upperBounds X := by
+    constructor
+    · -- intro p ⟨hp₁, hp₂⟩
+      simp [Set.mem_def, X]
+      constructor
+      · -- 0 < r + ε
+        linarith
+      · exact hε₂
+    · -- r + ε ∈ upperBounds X
+      exact r_add_ε_in_ub ε r X hε₁ h₀.left
+
+  have := @h₁ (r + ε) h₀'.left
+  have con : ¬ (r + ε ≤ r) := by linarith
+
+  contradiction
+
+-- Show that the set X has supremum (sup X)^3 = 2
+theorem part2 (r : ℝ) : IsLUB X r → r ^ 3 = 2 := by
   intro h
   rw [le_antisymm_iff]
-  refine ⟨step21 r h, step22 r h⟩
+  refine ⟨step21 r h, step22 r h ⟩
 
 end p2
 
