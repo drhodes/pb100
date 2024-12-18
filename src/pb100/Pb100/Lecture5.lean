7d9a78b6bde0406513e8983b6c749f9c9538b21d
Derek Rhodes <physci@gmail.com>
2024-12-18 16:25:08 -0500

    reverts lec5 thm3, the new version was proving something different

--- src/pb100/Pb100/Lecture5.lean
+++ src/pb100/Pb100/Lecture5.lean
@@ -19,9 +19,9 @@ namespace thm_2
 theorem density_of_rationals (x y : ℝ) (hx : x < y) : ∃ r : ℚ, x < ↑r ∧ ↑r < y := by
   let m := (x + y) / 2
   obtain h | h | h := lt_trichotomy m 0
-  · done
-  · done
-  · done
+  · sorry
+  · sorry
+  · sorry
 
 end thm_2
 
@@ -30,9 +30,12 @@ end thm_2
 
 namespace thm_3
 
-def S : Set ℝ := {1 - 1 / n | n > 0 }
+noncomputable
+def f (n : ℕ) : ℝ := 1 - 1 / n
 
-lemma step2 (x : ℝ) (hx : x < 1) : ∃ m, 0 < (m:ℕ) ∧ x < 1 - 1 / m := by
+def S := map f {n | n > 0 }
+
+lemma step3 (x : ℝ) (hx : x < 1) : ∃ m, 0 < (m:ℕ) ∧ x < 1 - 1 / m := by
   have ha := Real.instArchimedean
   rw [archimedean_iff_nat_lt] at ha
 
@@ -44,7 +47,7 @@ lemma step2 (x : ℝ) (hx : x < 1) : ∃ m, 0 < (m:ℕ) ∧ x < 1 - 1 / m := by
 
   have h₂ : 1 < p * (1 - x) := by exact (mul_inv_lt_iff₀ hx₁).mp hp
   have pos₁ : 0 < 1 - x := by nlinarith
-  have pos₂ : (0:ℝ) < 1 := by norm_num
+  have pos₂ : 0 < (1:ℝ) := by norm_num
   have pos₃ : (0:ℝ) < p := by
     have := pos_of_lt_mul pos₂ pos₁ h₂
     aesop
@@ -56,27 +59,31 @@ lemma step2 (x : ℝ) (hx : x < 1) : ∃ m, 0 < (m:ℕ) ∧ x < 1 - 1 / m := by
     nlinarith
   · linarith
 
-lemma step1 (x : ℝ) (h: x < 1) : x ∉ upperBounds S := by
-  simp [S, upperBounds]
-  have ⟨m, h₁, h₂⟩ := step2 x h
+
+lemma step2 (x : ℝ) : x < 1 → x ∉ upperBounds S := by
+  intro hx₁
+  simp [S, f, upperBounds]
+  have ⟨m, h₁, h₂⟩ := step3 x hx₁
   use m
   constructor
-  · simpa
-  · simp at h₂; linarith
+  · exact h₁
+  · --
+    simp at h₂
+    linarith
 
+lemma step1 : 1 ∈ upperBounds S := by
+  dsimp [upperBounds, S]
+  simp [f]
 
 theorem theorem_3 : IsLUB S 1 := by
   constructor
   · -- 1 ∈ upperBounds S
-    simp [upperBounds, S]
-    intro a ha
-    linarith
-
+    exact step1
   · -- 1 ∈ lowerBounds (upperBounds S)
     intro x hx
     by_contra hc
     push_neg at hc
-    have := step1 x hc
+    have := step2 x hc
     contradiction
 
 end thm_3
